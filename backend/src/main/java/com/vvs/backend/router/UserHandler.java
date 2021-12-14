a6afdfe8fa6da698dd2d05b5a0531b67f96ca0af
Victor <victorsmirnov@me.com>
2021-12-14 21:55:26 +0200

    add User service

--- backend/src/main/java/com/vvs/backend/router/UserHandler.java
+++ backend/src/main/java/com/vvs/backend/router/UserHandler.java
@@ -1,8 +1,36 @@
 package com.vvs.backend.router;
 
-import org.springframework.stereotype.Component;
+import com.vvs.backend.dto.UserDTO;
+import com.vvs.backend.service.JWTUtil;
+import com.vvs.backend.service.UserService;
 
-// @Component
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import reactor.core.publisher.Mono;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+
+@Component
 public class UserHandler {
   
+  @Autowired
+  private UserService userService;
+  @Autowired
+  private JWTUtil jwtUtil;
+
+  public Mono<ServerResponse> getUser(ServerRequest request) {
+
+    String token = request.headers().firstHeader("authorization").substring(7);
+
+    Mono<?> response = jwtUtil.getAllClaimsFromToken(token)
+      .flatMap(credentials -> userService.getUser(credentials.getSubject()).cast(UserDTO.class)
+      .map(userDetails -> userDetails));
+
+    return ServerResponse
+      .ok()
+      .contentType(APPLICATION_JSON)
+      .body(response, UserDTO.class);
+  }
 }
--- backend/src/main/java/com/vvs/backend/router/UserRouter.java
+++ backend/src/main/java/com/vvs/backend/router/UserRouter.java
@@ -1,5 +1,20 @@
 package com.vvs.backend.router;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.*;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+
+@Configuration
 public class UserRouter {
   
+  @Bean
+  public RouterFunction<ServerResponse> userRouterFunction(UserHandler userHandler) {
+    return RouterFunctions
+      .route(GET("/user").and(accept(APPLICATION_JSON)), userHandler::getUser);
+  }
 }
